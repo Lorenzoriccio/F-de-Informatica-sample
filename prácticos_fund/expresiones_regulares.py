893b4d5840c2c09ba1069afc9e22dc092b91700a
Lorenzoriccio <lorenzo.riccio.2002@gmail.com>
2022-04-15 14:33:18 -0300

    hola

--- prácticos_fund/expresiones_regulares.py
+++ prácticos_fund/expresiones_regulares.py
@@ -1,9 +1,11 @@
 # EJERCICIO 1
 import re 
-string = "Profesora del 2022: Ana Julia Velez Rueda"
+string = "1. Profesora de la metria: Ana Julia Velez Rueda"
 re.search('\w', string) #funciona porque quiero encontrar al menos 1, pero está ok?
 
 # EJERCICIO 2
 import re 
-texto = "Tutor de la materia: Guillermo Benitez"
+texto = "2. Tutor de la materia: Guillermo Benitez"
 re.search('\W', texto)
+
+# EJERCICIO 3 
