1d68733e47dc992083fdc04b11c7661b64e8469a
Lorenzoriccio <lorenzo.riccio.2002@gmail.com>
2022-04-15 22:09:29 -0300

    4

--- prácticos_fund/expresiones_regulares.py
+++ prácticos_fund/expresiones_regulares.py
@@ -9,3 +9,8 @@ texto = "2. Tutor de la materia: Guillermo Benitez"
 re.search('\W', texto)
 
 # EJERCICIO 3 
+# EJERCICIO 4
+import re
+string = "Defíni la función aprobar_materias"
+patron = '\w' + '_' + '\w'
+re.search(patron, string) 
