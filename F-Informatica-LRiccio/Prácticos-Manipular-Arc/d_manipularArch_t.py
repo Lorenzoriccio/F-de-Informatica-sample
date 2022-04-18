a786cafeff66df1bc60d9e8ecac1165bbb854743
Lorenzoriccio <lorenzo.riccio.2002@gmail.com>
2022-04-18 17:53:25 -0300

    2

--- F-Informatica-LRiccio/Prácticos-Manipular-Arc/d_manipularArch_t.py
+++ F-Informatica-LRiccio/Prácticos-Manipular-Arc/d_manipularArch_t.py
@@ -9,4 +9,8 @@
 El comando de os.listdir() te lista los archivos de ti biblioteca ("List directory") '''
 
 # DESAFÍO III
+# 🧗‍♀️ Desafío III: Abrí el archivo bio.txt y escribí una mini biografía de presentación. Para pensar 🤔:¿Cómo darías formato al texto que estas creando?
 # DESAFÍO IV
+'''🧗‍♀️Desafio IV: Descargá el archivo manipulacion_archivos.txt y creá un programa que lea su contenido y lo imprima en pantalla el resultado.
+Para pensar 🤔: ¿Creés que habrá una forma más práctica de leer archivos estructurados o tabulados?
+¡Te dejo con la intriga hasta nuestro capítulo de análisis de datos! Por ahora, hasta aquí llegamos 👋'''
