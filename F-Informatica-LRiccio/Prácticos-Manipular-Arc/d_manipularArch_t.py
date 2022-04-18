7c0becde64ea31a20121e9c145947536e2088c9c
Lorenzoriccio <lorenzo.riccio.2002@gmail.com>
2022-04-18 19:44:38 -0300

    desafios

--- F-Informatica-LRiccio/Prácticos-Manipular-Arc/d_manipularArch_t.py
+++ F-Informatica-LRiccio/Prácticos-Manipular-Arc/d_manipularArch_t.py
@@ -1,8 +1,9 @@
 # DESAFÍO I
 # 🧗‍♀️ Desafío I: Creá un archivo de prueba (bio.txt) en la carpeta destinada a los prácticos de la materia.
-'''bio = (r"C:\Users\Usuario\Desktop\UNIVERSIDAD\fund_info\F-de-Informatica-sample\F-Informatica-LRiccio\Prácticos-Manipular-Arc")
-with open(bio, 'w') as file: 
-    file.write("Mi nombre es Lorenzo") '''
+'''
+## Primero (antes de entrar a python) entras con cd a la carpeta 
+with open('bio.txt', 'w') as file: 
+    file.write("Mi nombre es Lorenzo Riccio y tengo 19 años. Estudio en la Ucema.") '''
 
 
 # DESAFÍO II
@@ -12,6 +13,7 @@ El comando de os.listdir() te lista los archivos de ti biblioteca ("List directo
 
 # DESAFÍO III
 # 🧗‍♀️ Desafío III: Abrí el archivo bio.txt y escribí una mini biografía de presentación. Para pensar 🤔:¿Cómo darías formato al texto que estas creando?
+# ya está hecho en el desafío 1
 # DESAFÍO IV
 '''🧗‍♀️Desafio IV: Descargá el archivo manipulacion_archivos.txt y creá un programa que lea su contenido y lo imprima en pantalla el resultado.
 Para pensar 🤔: ¿Creés que habrá una forma más práctica de leer archivos estructurados o tabulados?
