f65358ff372a54744f88b280841d9339617405c6
Lorenzoriccio <lorenzo.riccio.2002@gmail.com>
2022-04-18 19:25:43 -0300

    ree

--- F-Informatica-LRiccio/Prácticos-REE/expresiones_regulares.py
+++ F-Informatica-LRiccio/Prácticos-REE/expresiones_regulares.py
@@ -1,28 +1,80 @@
 # EJERCICIO 1
+# Escribí un programa que verifique si un string tiene al menos un carácter permitido. Estos caracteres son a-z, A-Z y 0-9.
 import re 
 string = "1. Profesora de la metria: Ana Julia Velez Rueda"
 re.search('\w', string) #funciona porque quiero encontrar al menos 1, pero está ok?
 
 # EJERCICIO 2
+#Escribí un programa que verifique si un string tiene todos sus caracteres permitidos. Estos caracteres son a-z, A-Z y 0-9.
 import re 
 texto = "2. Tutor de la materia: Guillermo Benitez"
 re.search('\W', texto)
 
 # EJERCICIO 3 
+'''Creá un programa que verifique las siguientes condiciones:
+si un string dado tiene una h seguida de ninguna o más e.
+si un string dado tiene una h seguida de una o más e.
+si un string dado tiene una h seguida de una o más e.
+si un string dado tiene una h seguida de dos o tres e.'''
+
 # EJERCICIO 4
+# Realizá un programa que encuentre una palabra unida a otra con un guión bajo en un string dado (el string no debe contener espacios).
 import re
 string = "Defíni la función aprobar_materias"
 patron = '\w' + '_' + '\w'
 re.search(patron, string) 
 
 # EJERICIO 5
+# Escribí un programa que diga si un string empieza con un número específico.
+import re
+string = input("ingrese una cadena de caracteres: ")
+patron = "^3"
+if re.match(patron, string) is not None:
+    print("el string comienza con el numero 3")
+else:
+    print("el string no emieza con el numero 3")
+
 # EJERICIO 6
+# Escribí un programa que dada una lista de strings verifique si se encuentran en una frase dada.
+import re
+strings = ["hola", "te", "hoy"]
+frase = "hola como te sentis hoy?"
+
+for palabra in strings:
+    if re.search(palabra, frase) is not None:
+        print("la palabra esta en la frase")
+    else:
+        print("la palabra no esta en la frase")
+
 # EJERICIO 7
+# Realizá un programa que encuentre un string que contenga solamente letras minúsculas, mayúsculas, espacios y números.
+import re
+string = input("ingrese una frase: ")
+patron = "[a-zA-Z0-9(\s)]"
+coincidencias = []
+
+for caracter in string:
+    coincidencias.append(re.findall(patron, caracter))
+    if len(coincidencias) == len(string):
+        print("todos los caracteres de la frase son o letras mayusculas, o letras minusculas, o numeros, o espacios")
+    else:
+        print("al menos un caracter no es una letra mayuscula o minuscula, numero o espacio")
+
 # EJERICIO 8
+# Escribí un programa que separe y devuelva los caracteres númericos de un string.
 # EJERICIO 9
+'''Escribí un programa que extraiga los caracteres que estén entre guiones en un string. (String de ejemplo: 
+"Hoy estuvimos trabajando con re -regular expression- en python -con VSCode-")'''
 # EJERICIO 10
+# Obtené las substrings y las posiciones de estas en una string dada considerando que las substrings están delimitadas por los caracteres @ o &.
 # EJERICIO 11
+'''Realizá un programa que dado una lista de strings verifique que dos palabras dentro del string empiecen con la letra P y las imprima. (Lista 
+de ejemplo: ["Práctica Python", "Práctica C++", "Práctica Fortran"]).'''
 # EJERICIO 12
+# Escribí un programa que reemplace todas las ocurrencias de espacios, guiones bajos y dos puntos por la barra vertical (|).
 # EJERICIO 13
+# Escribí un programa que reemplace los dos primeros caracteres no alfanúmericos por guiones bajos.
 # EJERICIO 14
+# Realizá un programa que reemplace los espacios y tabulaciones por punto y coma.
 # EJERICIO 15
+# Realizá un programa que validar si una cuenta de mail está escrita correctamente.
