deab578b9bebc5807bc42da7582332d73562f42e
Lorenzoriccio <lorenzo.riccio.2002@gmail.com>
2022-04-18 21:52:26 -0300

    manipular archivos desafíos

--- F-Informatica-LRiccio/Prácticos-Manipular-Arc/d_manipularArch_t.py
+++ F-Informatica-LRiccio/Prácticos-Manipular-Arc/d_manipularArch_t.py
@@ -15,6 +15,9 @@ El comando de os.listdir() te lista los archivos de ti biblioteca ("List directo
 # 🧗‍♀️ Desafío III: Abrí el archivo bio.txt y escribí una mini biografía de presentación. Para pensar 🤔:¿Cómo darías formato al texto que estas creando?
 # ya está hecho en el desafío 1
 # DESAFÍO IV
-'''🧗‍♀️Desafio IV: Descargá el archivo manipulacion_archivos.txt y creá un programa que lea su contenido y lo imprima en pantalla el resultado.
-Para pensar 🤔: ¿Creés que habrá una forma más práctica de leer archivos estructurados o tabulados?
-¡Te dejo con la intriga hasta nuestro capítulo de análisis de datos! Por ahora, hasta aquí llegamos 👋'''
+# 🧗‍♀️Desafio IV: Descargá el archivo manipulacion_archivos.txt y creá un programa que lea su contenido y lo imprima en pantalla el resultado.
+
+'''
+with open('manipulacion_archivos.txt', 'r') as file: 
+    file.readlines() '''
+
